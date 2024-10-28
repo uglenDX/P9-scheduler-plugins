775a6582f441f258b37c6295861bca10997d4960
Simon Andersen <smpa20@student.aau.dk>
2024-10-28 11:47:01 +0100

    added timestamp endpoint

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import requests
 from flask import Flask, jsonify, request
 
 # Load the data
-data = pd.read_csv(".\data\ElectricityProdex5MinRealtime.csv", delimiter=";")
+data = pd.read_csv(".\data\ElectricityProdex5MinRealtime.csv", delimiter=";", decimal=",")
 data = data.drop(columns=
                  ["ExchangeGreatBelt", 
                   "ExchangeGermany", 
@@ -12,12 +12,15 @@ data = data.drop(columns=
                   "ExchangeNorway", 
                   "ExchangeSweden", 
                   "BornholmSE4"])
-data["SumProduction"] = data["ProductionLt100MW"] + data["ProductionGe100MW"] + data["OffshoreWindPower"] + data["OnshoreWindPower"] + data["SolarPower"]
+#data["SumProduction"] = data["ProductionLt100MW"] + data["ProductionGe100MW"] + data["OffshoreWindPower"] + data["OnshoreWindPower"] + data["SolarPower"]
+data["SumProduction"] = data[["ProductionLt100MW", "ProductionGe100MW", "OffshoreWindPower", "OnshoreWindPower", "SolarPower"]].sum(axis=1, numeric_only=True)
+
 #print(data.count())
 #print(data.head())
 
 app = Flask(__name__)
 
-# @app.route("/data")
-# def test_data():
-#     return jsonify(data.to_dict())
+@app.route("/data/<timestamp>", methods=["GET"])
+def test_data(timestamp):
+    row = data.loc[data["Minutes5DK"] == timestamp]
+    return row.to_json(orient="records")
