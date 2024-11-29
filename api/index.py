f5691e890dcbe7f0007603c8323a3615fe4758ce
Simon Andersen <smpa20@student.aau.dk>
2024-11-29 13:46:43 +0100

    added more countries

--- api/index.py
+++ api/index.py
@@ -3,11 +3,9 @@ import datetime
 from flask import Flask
 
 # Load the data
-denmark_residential_wt = pd.read_csv("./data/denmark-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
-spain_residential_wt = pd.read_csv("./data/spain-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
-austria_residential_wt = pd.read_csv("./data/austria-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
-
-
+denmark_residential_wt = pd.read_csv(".\..\data\denmark-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
+spain_residential_wt = pd.read_csv(".\..\data\spain-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
+austria_residential_wt = pd.read_csv(".\..\data\\austria-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
 
 data = pd.DataFrame(denmark_residential_wt, columns=['Time', 'Generic 100kWh Li-Ion State of Charge', 'Total Renewable Power Output'])
 data.rename(columns={'Generic 100kWh Li-Ion State of Charge': 'Battery_charge',
@@ -23,8 +21,9 @@ data_austria = pd.DataFrame(austria_residential_wt, columns=['Time', 'Generic 10
 data_austria.rename(columns={'Generic 100kWh Li-Ion State of Charge': 'Battery_charge',
                      'Total Renewable Power Output': 'Renewable_output' },
                      inplace=True)
-# print(data.head())
-# print(data.dtypes)
+
+print(data.head())
+print(data.dtypes)
 
 app = Flask(__name__)
 
@@ -35,19 +34,23 @@ def home():
 @app.route("/data", methods=["GET"])
 def state_of_charge():
     server_time = datetime.datetime.now()
-    #print(server_time)
+    print(server_time)
     rounded_time = roundDownDateTime(server_time)
-    #print(rounded_time)
+    print(rounded_time)
 
     row = data.loc[data["Time"] == rounded_time]
     row["Location"] = ["Denmark"]
 
     row2 = data_spain.loc[data_spain["Time"] == rounded_time]
+    row2["Location"] = ["Spain"]
+
     row3 = data_austria.loc[data_austria["Time"] == rounded_time]
+    row3["Location"] = ["Austria"]
 
     combined_rows = pd.concat([row, row2, row3])
+    return combined_rows.to_json(orient="records")
 
-    return row.to_json(orient="records")
+    #return row.to_json(orient="records")
 
 @app.route("/alive", methods=["GET"])
 def alive():
@@ -58,4 +61,4 @@ def roundDownDateTime(dt):
     delta_min = dt.minute % 5
     rounded_dt = datetime.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute - delta_min)
     # Remember to change year according to data
-    return rounded_dt.strftime("%m/%d/2023 %H:%M:%S %p")
+    return rounded_dt.strftime("%m/%d/2023 %I:%M:%S %p").lstrip("0").replace(" 0", " ")
