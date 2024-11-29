802076532e06517d91e8260d67b30e4e7c6349da
Simon Andersen <smpa20@student.aau.dk>
2024-11-29 10:10:50 +0100

    working stuff

--- app.py
+++ app.py
@@ -1,31 +1,27 @@
 import pandas as pd
-import requests
 import datetime
 from flask import Flask, jsonify, request
 
 # Load the data
-data = pd.read_csv(".\data\ElectricityProdex5MinRealtime.csv", delimiter=";", decimal=",")
-data = data.drop(columns=
-                 ["ExchangeGreatBelt", 
-                  "ExchangeGermany", 
-                  "ExchangeNetherlands", 
-                  "ExchangeGreatBritain", 
-                  "ExchangeNorway", 
-                  "ExchangeSweden", 
-                  "BornholmSE4"])
-#data["SumProduction"] = data["ProductionLt100MW"] + data["ProductionGe100MW"] + data["OffshoreWindPower"] + data["OnshoreWindPower"] + data["SolarPower"]
-data["SumProduction"] = data[["ProductionLt100MW", "ProductionGe100MW", "OffshoreWindPower", "OnshoreWindPower", "SolarPower"]].sum(axis=1, numeric_only=True)
+denmark_residential_wt = pd.read_csv(".\data\denmark-residential-wt_detailed_timeseries_001_00006.csv", delimiter=",", decimal=".")
 
-#print(data.count())
-#print(data.head())
+data = pd.DataFrame(denmark_residential_wt, columns=['Time', 'Generic 100kWh Li-Ion State of Charge', 'Total Renewable Power Output'])
+data.rename(columns={'Generic 100kWh Li-Ion State of Charge': 'Battery_charge',
+                     'Total Renewable Power Output': 'Renewable_output' },
+                     inplace=True)
+print(data.head())
+print(data.dtypes)
 
 app = Flask(__name__)
 
-@app.route("/data/<timestamp>", methods=["GET"])
-def test_data(timestamp):
+@app.route("/data", methods=["GET"])
+def state_of_charge():
+    server_time = datetime.datetime.now()
+    #print(server_time)
+    rounded_time = roundDownDateTime(server_time)
+    #print(rounded_time)
 
-    timestamp = roundDownDateTime(datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M"))
-    row = data.loc[data["Minutes5DK"] == timestamp]
+    row = data.loc[data["Time"] == rounded_time]
 
     return row.to_json(orient="records")
 
@@ -34,17 +30,8 @@ def alive():
     print("I'm alive!")
     return "I'm alive!"
 
-@app.route("/print", methods=["POST"])
-def process_json():
-    content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        json = request.json
-        print(json)
-        return json
-    else:
-        return 'Content-Type not supported!'
-
 def roundDownDateTime(dt):
     delta_min = dt.minute % 5
     rounded_dt = datetime.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute - delta_min)
-    return rounded_dt.strftime("%Y-%m-%d %H:%M")
+    # Remember to change year according to data
+    return rounded_dt.strftime("%m/%d/2023 %H:%M:%S %p")
