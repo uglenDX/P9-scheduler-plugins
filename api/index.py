c79230233e045151ba5dde7e43a682de62cec2a9
Simon Andersen <smpa20@student.aau.dk>
2024-11-29 13:24:44 +0100

    testomg in prod

--- api/index.py
+++ api/index.py
@@ -3,14 +3,28 @@ import datetime
 from flask import Flask
 
 # Load the data
-denmark_residential_wt = pd.read_csv("./data/denmark-residential-wt_detailed_timeseries_001_00006.csv", delimiter=",", decimal=".")
+denmark_residential_wt = pd.read_csv("./data/denmark-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
+spain_residential_wt = pd.read_csv("./data/spain-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
+austria_residential_wt = pd.read_csv("./data/austria-residential-wt_detailed_timeseries.csv", delimiter=",", decimal=".")
+
+
 
 data = pd.DataFrame(denmark_residential_wt, columns=['Time', 'Generic 100kWh Li-Ion State of Charge', 'Total Renewable Power Output'])
 data.rename(columns={'Generic 100kWh Li-Ion State of Charge': 'Battery_charge',
                      'Total Renewable Power Output': 'Renewable_output' },
                      inplace=True)
-print(data.head())
-print(data.dtypes)
+
+data_spain = pd.DataFrame(spain_residential_wt, columns=['Time', 'Generic 100kWh Li-Ion State of Charge', 'Total Renewable Power Output'])
+data_spain.rename(columns={'Generic 100kWh Li-Ion State of Charge': 'Battery_charge',
+                     'Total Renewable Power Output': 'Renewable_output' },
+                     inplace=True)
+
+data_austria = pd.DataFrame(austria_residential_wt, columns=['Time', 'Generic 100kWh Li-Ion State of Charge', 'Total Renewable Power Output'])
+data_austria.rename(columns={'Generic 100kWh Li-Ion State of Charge': 'Battery_charge',
+                     'Total Renewable Power Output': 'Renewable_output' },
+                     inplace=True)
+# print(data.head())
+# print(data.dtypes)
 
 app = Flask(__name__)
 
@@ -26,6 +40,12 @@ def state_of_charge():
     #print(rounded_time)
 
     row = data.loc[data["Time"] == rounded_time]
+    row["Location"] = ["Denmark"]
+
+    row2 = data_spain.loc[data_spain["Time"] == rounded_time]
+    row3 = data_austria.loc[data_austria["Time"] == rounded_time]
+
+    combined_rows = pd.concat([row, row2, row3])
 
     return row.to_json(orient="records")
 
